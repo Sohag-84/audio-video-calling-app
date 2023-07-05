8a9067546846a98f56516b4a17fdacd3d8b64186
Sohag <ihsohag-84@gmail.com>
2023-07-05 13:05:58 +0600

    audio video call integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:video_conference/views/home_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:video_conference/views/home_page.dart';
 
-void main() {
+Future<void> main() async{
+  await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
 
@@ -18,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const HomePage(),
     );
   }
 }
